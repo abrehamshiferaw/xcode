da803127bab55370f0e398e42b321f973727b9da
Abreham <abrishwon9@gmail.com>
2026-04-12 12:26:21 +0300

    Initial Commit

---
+++ SkyLens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SkyLens
+//
+//  Created by abrish on 12/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SkyLens/SkyLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyLensApp.swift
+//  SkyLens
+//
+//  Created by abrish on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SkyLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
